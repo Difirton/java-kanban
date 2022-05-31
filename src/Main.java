74e9b73d0109ee5a9689bfcd1e547b6eee98cd36
Dmitriy Kruglov <difirton@yandex.ru>
2022-05-31 16:16:23 +0300

    <feat> - add CustomLinkedList

--- src/Main.java
+++ src/Main.java
@@ -4,69 +4,19 @@ import service.TasksManager;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        Я даже не сомневался, что Вы напишете, что перемудрил, но задание было слишком маленьким для меня, а я тут еще
-        прочитал недавно книгу по патернам проектировани и решил попробовать )
-         */
-
         TasksManager tasksManager = Manager.getDefault();
-        tasksManager.createNewEpic("Epic 1", "Desc 1");
-        tasksManager.createNewSubtask("Subtask 1.1", "Desc sub 1", 1L);
-        tasksManager.createNewSubtask("Subtask 1.2", "Desc sub 1", 1L);
-        tasksManager.createNewSubtask("Subtask 1.3", "Desc sub 1", 1L);
-        tasksManager.createNewEpic("Epic 2", "Desc 2");
-        tasksManager.createNewSubtask("Subtask 2.1", "Desc sub 2", 2L);
-        tasksManager.createNewSubtask("Subtask 2.2", "Desc sub 2", 2L);
+        tasksManager.createNewEpic("Epic 1", "Description Epic 1");
+        tasksManager.createNewEpic("Epic 2", "Description Epic 2");
+        tasksManager.createNewEpic("Epic 2", "Description Epic 3");
+//        tasksManager.createNewSubtask("Subtask 1", "Subtask 1", 1L);
+//        tasksManager.createNewSubtask("Subtask 2", "Subtask 2", 1L);
+//        tasksManager.createNewSubtask("Subtask 3", "Subtask 3", 2L);
+//        tasksManager.createNewSubtask("Subtask 4", "Subtask 4", 2L);
+//        tasksManager.createNewSubtask("Subtask 5", "Subtask 5", 2L);
+        tasksManager.getEpicById(1L);
+        tasksManager.getEpicById(2L);
+        tasksManager.getEpicById(3L);
+        System.out.println(tasksManager.getHistory());
 
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getHistory().forEach(System.out::println);
-        System.out.println(tasksManager.getHistory().size());
-        System.out.println("_________________________________________________");
-        tasksManager.getEpicById(2L);
-        tasksManager.getAllSubtasks();
-        tasksManager.getSubtaskByIdOrNull(5L);
-        tasksManager.getSubtaskByIdOrNull(3L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getHistory().forEach(System.out::println);
-        System.out.println(tasksManager.getHistory().size());
-        System.out.println("_________________________________________________");
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getEpicById(1L);
-        tasksManager.getHistory().forEach(System.out::println);
-        System.out.println(tasksManager.getHistory().size());
-        System.out.println("_________________________________________________");
-        tasksManager.removeEpicById(2L);
-        tasksManager.getEpicById(2L);
-        tasksManager.getSubtaskByIdOrNull(4L);
-        tasksManager.getSubtaskByIdOrNull(5L);
-        tasksManager.getSubtaskByIdOrNull(2L);
-        tasksManager.getSubtaskByIdOrNull(2L);
-        tasksManager.getHistory().forEach(System.out::println);
-        System.out.println(tasksManager.getHistory().size());
     }
 }
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -2,27 +2,121 @@ package service;
 
 import entity.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int LIMIT_HISTORY_QUEUE_TASKS = 10;
-    private final List<Task> historyQueueTasks = new LinkedList<>();
-    /* В LinkedList нет конструктора с интовым аргументом для указания емкости, он же просто подвязывает двойные ссылки
-    к смежным элементам, емкость есть у ArrayList, но его DEFAULT_CAPACITY итак равна 10 */
+    CustomLinkedList historyQueueTasks = new CustomLinkedList();
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            historyQueueTasks.add(task);
-        }
-        while (historyQueueTasks.size() > LIMIT_HISTORY_QUEUE_TASKS) {
-            historyQueueTasks.remove(0);
-        }
+        //TODO подумать о хранении id не подходит
+        historyQueueTasks.add(task);
+    }
+
+    @Override
+    public void remove(Long id) {
+        historyQueueTasks.remove(id);
     }
 
     @Override
     public List<Task> getHistory() {
-        return historyQueueTasks;
+        return historyQueueTasks.toList();
+    }
+
+    class CustomLinkedList {
+        private Map<Long, Node> entryMap;
+        private Long head;
+        private Long tail;
+
+        public CustomLinkedList() {
+            this.entryMap = new HashMap<>();
+        }
+
+        void add(Task task) {
+            Long id = task.getId();
+            if (this.head == null) {
+                Node node = new Node(task);
+                this.entryMap.put(id, node);
+                this.head = id;
+                this.tail = id;
+            } else {
+                if (entryMap.containsKey(id)){
+                    Node node = entryMap.get(id);
+                    rebindingLinksAfterTaskExtraction(id);
+                    node.setHead(this.tail);
+                    node.setTail(null);
+                    this.tail = id;
+                } else {
+                    Node newTail = new Node(task, this.tail);
+                    this.entryMap.get(this.tail).setTail(id);
+                }
+            }
+        }
+
+        void remove(Long id) {
+            rebindingLinksAfterTaskExtraction(id);
+            this.entryMap.remove(id);
+        }
+
+        List<Task> toList() {
+            List<Task> taskList = new ArrayList<>();
+            Node node = this.entryMap.getOrDefault(this.head, null);
+            while (node != null) {
+                taskList.add(node.getTask());
+                node = this.entryMap.get(node.getTail());
+            }
+            return taskList;
+        }
+
+        private void rebindingLinksAfterTaskExtraction(Long id) {
+            Node removedNode= entryMap.get(id);
+            Long tail = removedNode.getTail();
+            Long head = removedNode.getHead();
+            entryMap.get(head).setTail(tail);
+            entryMap.get(tail).setHead(head);
+        }
+
+        private class Node{
+            final Task task;
+            Long head;
+            Long tail;
+
+            private Node(Task task) {
+                this.task = task;
+                this.head = null;
+                this.tail = null;
+
+            }
+
+            private Node(Task task, Long head) {
+                this.task = task;
+                this.head = head;
+                this.tail = null;
+            }
+
+            private Task getTask() {
+                return task;
+            }
+
+            private Long getHead() {
+                return head;
+            }
+
+            private void setHead(Long head) {
+                this.head = head;
+            }
+
+            private Long getTail() {
+                return tail;
+            }
+
+            private void setTail(Long tail) {
+                this.tail = tail;
+            }
+
+            public final String toString() {
+                return task.toString() + head + "^" + tail;
+            }
+        }
     }
 }
