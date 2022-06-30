e90a8441b69519916686cced656d1d605172a94f
Dmitriy Kruglov <difirton@yandex.ru>
2022-06-30 23:53:28 +0300

    <feat> - add read directory from properties

--- src/test/java/service/FileBackedTasksManagerTest.java
+++ src/test/java/service/FileBackedTasksManagerTest.java
@@ -1,24 +1,29 @@
 package service;
 
 import constant.TypeTasksManager;
+import error.ManagerSaveException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FileBackedTasksManagerTest {
     private TasksManager fileBackedTasksManager;
     private File testFile;
+    String pathToTestSaveAndLoadData;
 
     @BeforeEach
     public void setUp() throws Exception{
         fileBackedTasksManager = Manager.getTaskManager(TypeTasksManager.FILE_BACKED_TASKS_MANAGER);
-        testFile = new File("src" + File.separator + "test" + File.separator + "resources" +
-                File.separator + "data" + File.separator + "dataTestFileBackedTasksManager.bin");
+        pathToTestSaveAndLoadData = readPathToSaveTestData();
+        testFile = new File(pathToTestSaveAndLoadData + File.separator + "dataFileBackedTasksManager.bin");
         fileBackedTasksManager.createNewEpic("Epic 1", "Desc 1");
         fileBackedTasksManager.createNewSubtask("Subtask 1.1", "Desc sub 1", 1L);
         fileBackedTasksManager.createNewSubtask("Subtask 1.2", "Desc sub 1", 1L);
@@ -28,6 +33,18 @@ public class FileBackedTasksManagerTest {
         fileBackedTasksManager.createNewSubtask("Subtask 2.2", "Desc sub 2", 5L);
     }
 
+    private String readPathToSaveTestData() {
+        try (FileInputStream propertiesReader = new FileInputStream("config.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesReader);
+            return properties.getProperty("test.directory.toSaveFileBackedTasksManager");
+        } catch (IOException exception) {
+            throw new ManagerSaveException("There is no data for the path where the object will be serialized. " +
+                    "Check that there is a config.properties file at the root of the project with the key " +
+                    "test.directory.toSaveFileBackedTasksManager" + exception.getMessage());
+        }
+    }
+
     @Test
     @DisplayName("Test save in file all parameters of fileBackedTasksManager, expected ok")
     public void testSave() throws NoSuchFieldException, IllegalAccessException {
@@ -35,8 +52,7 @@ public class FileBackedTasksManagerTest {
         field.setAccessible(true);
         field.set(fileBackedTasksManager, testFile);
         fileBackedTasksManager.getEpicById(1L);
-        File actualFile = new File("src" + File.separator + "test" + File.separator + "resources" +
-                File.separator + "data" + File.separator + "dataTestFileBackedTasksManager.bin");
+        File actualFile = new File(pathToTestSaveAndLoadData + File.separator + "dataFileBackedTasksManager.bin");
         assertTrue(actualFile.exists());
         actualFile.delete();
     }
